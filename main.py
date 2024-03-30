2b099d78c61b08889c064e6ef4367c8384103238
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-29 19:49:44 -0500

    main and resolvers

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+import strawberry
+from resolvers import QueryComment
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+schema= strawberry.Schema(query=QueryComment)
