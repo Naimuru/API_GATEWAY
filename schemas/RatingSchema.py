9c674a7416e79455e71bdab455cc840916a752da
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-31 15:26:11 -0500

    port fixed for rating ms

--- schemas/RatingSchema.py
+++ schemas/RatingSchema.py
@@ -2,7 +2,7 @@ import typing
 import strawberry
 import datetime
 from utilities import *
-from server import API_URL
+from server import RATING_URL_BASE
 from ms_types.RatingTypes import *
 
 
@@ -11,27 +11,27 @@ class QueryRating:
         
     @strawberry.field
     def rating(self,idUser:str,idItem:str)-> typing.List[Rating]:
-        return generalRequest(API_URL+"calification/itemUser/{0}/{1}".format(idUser,idItem),GET)
+        return generalRequest(RATING_URL_BASE+"calification/itemUser/{0}/{1}".format(idUser,idItem),GET)
     @strawberry.field
     def ratings(self)->typing.List[Rating]:
-        return generalRequest(API_URL+"calification/all",GET)
+        return generalRequest(RATING_URL_BASE+"calification/all",GET)
     @strawberry.field
     def average(self,idItem:str)-> str:
-        return generalRequest(API_URL+"calification/average/{0}".format(idItem),GET)
+        return generalRequest(RATING_URL_BASE+"calification/average/{0}".format(idItem),GET)
 
 @strawberry.type
 class MutationsRating:
 
     @strawberry.mutation
     def createRating(self,rating:RatingInput) ->Rating:
-        response = generalRequest(API_URL+"calification/add",POST,body=strawberry.asdict(rating))
+        response = generalRequest(RATING_URL_BASE+"calification/add",POST,body=strawberry.asdict(rating))
         return response
 
     @strawberry.mutation
     def updateRating(self,idUser:str,idItem:str,rating:RatingUpdate) -> str:
-        return generalRequest(API_URL+"calification/{0}/{1}".format(idUser,idItem),PATCH,body=strawberry.asdict(rating))
+        return generalRequest(RATING_URL_BASE+"calification/{0}/{1}".format(idUser,idItem),PATCH,body=strawberry.asdict(rating))
 
     @strawberry.mutation
     def deleteRating(self,idUser:str,idItem:str) -> str:
-        response=generalRequest(API_URL+"calification/{0}/{1}".format(idUser,idItem),DELETE)
+        response=generalRequest(RATING_URL_BASE+"calification/{0}/{1}".format(idUser,idItem),DELETE)
         return response    
