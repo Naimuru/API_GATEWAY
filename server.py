34ac613f29489a24331a673536e82873c8ec9daa
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-31 14:53:29 -0500

    UserSocialMS URL

--- server.py
+++ server.py
@@ -5,4 +5,6 @@ base_endpoint = "api/v1"
 
 # Now you can use base_url, endpoint, and port in your code
 comments_port="8080"
-API_URL="http://{0}:{1}/{2}/".format(base_url,comments_port,base_endpoint)
+userSocial_port="8000"
+COMMENTS_URL_BASE="http://{0}:{1}/{2}/".format(base_url,comments_port,base_endpoint)
+USERSOCIAL_URL_BASE="http://{0}:{1}/{2}/".format(base_url,userSocial_port,base_endpoint)
