f37fac4b05fcc8478c313983a0a5dd577203227b
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-30 23:48:44 -0500

    api url constant fixed

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ base_endpoint = "api/v1"
 
 # Now you can use base_url, endpoint, and port in your code
 comments_port="8080"
-COMMENTS_API="http://{0}:{1}/{2}/".format(base_url,comments_port,base_endpoint)
+API_URL="http://{0}:{1}/{2}/".format(base_url,comments_port,base_endpoint)
