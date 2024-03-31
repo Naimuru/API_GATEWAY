8740e72347d62adc789e59c46ec3cf48be681194
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-31 01:09:52 -0500

    http constants

--- utilities.py
+++ utilities.py
@@ -1,21 +1,26 @@
 import requests
 import os
 import json
+GET="GET"
+POST="POST"
+PATCH="PATCH"
+PUT="PUT"
+DELETE="DELETE"
 def generalRequest(url, method, body=None, full_response=False):
     headers = {'Content-Type': 'application/json'}
     if os.getenv('SHOW_URLS'):
         print(url)
 
     try:
-        if method == 'GET':
+        if method == GET:
             response = requests.get(url, headers=headers)
-        elif method == 'POST':
+        elif method == POST:
             response = requests.post(url, json=body, headers=headers)
-        elif method == 'PATCH':
+        elif method == PATCH:
             response = requests.patch(url, json=body, headers=headers)
-        elif method == 'PUT':
+        elif method == PUT:
             response = requests.put(url, json=body, headers=headers)
-        elif method == 'DELETE':
+        elif method == DELETE:
             response = requests.delete(url, headers=headers)
         else:
             raise ValueError(f'Invalid method: {method}')
