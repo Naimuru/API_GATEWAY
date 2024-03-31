138d6c7d75e8828a15eedc012216778c53c5adbb
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-31 01:10:08 -0500

    comments mutations

--- schemas/CommentsSchema.py
+++ schemas/CommentsSchema.py
@@ -30,26 +30,35 @@ class CommentInput:
     content: str
     itemMusicId: str
 
+@strawberry.input
+class ReplyInput:
+    content: str
+    userId: str
+    itemMusicId: typing.Optional[str]=None
+    parentId: typing.Optional[str]=None
+@strawberry.type
+class DeleteCommentResponse:
+    acknowledged: str
+    deletedCount: int
 @strawberry.type
 class Query:
         
     @strawberry.field
     def comment(self,id:str)-> Comment:
-        #return generalRequest(url_comments+id,"GET")
-        return generalRequest(API_URL+"comments/{0}".format(id),"GET")
+        return generalRequest(API_URL+"comments/{0}".format(id),GET)
     @strawberry.field
     def replies(self,parentId:str)->typing.List[Comment]:
-        return generalRequest(API_URL+"comments/{0}/replies".format(parentId),"GET")
+        return generalRequest(API_URL+"comments/{0}/replies".format(parentId),GET)
     
     @strawberry.field
     def comments(self)->typing.List[Comment]:
-        return generalRequest(API_URL+"comments/","GET")
+        return generalRequest(API_URL+"comments/",GET)
     @strawberry.field
     def itemComments(self,itemMusicId:str)->typing.List[Comment]:
-        return generalRequest(API_URL+"item/{0}/comments".format(itemMusicId),"GET")
+        return generalRequest(API_URL+"item/{0}/comments".format(itemMusicId),GET)
     @strawberry.field
     def userComments(self,userId:str)->typing.List[Comment]:
-        return generalRequest(API_URL+"user/{0}/comments".format(userId),"GET")
+        return generalRequest(API_URL+"user/{0}/comments".format(userId),GET)
     
     
     
@@ -59,29 +68,22 @@ class Mutations:
 
     @strawberry.mutation
     def updateComment(self,id:str,comment:CommentUpdate) -> Comment:
-        return generalRequest(API_URL+"comments/{0}".format(id),"PATCH",body=strawberry.asdict(comment))
+        return generalRequest(API_URL+"comments/{0}".format(id),PATCH,body=strawberry.asdict(comment))
     
     @strawberry.mutation
     def createComment(self,comment:CommentInput) ->Comment:
-        return generalRequest(API_URL+"comments/","POST",body=strawberry.asdict(comment))
-
-        
-
-# def jsonToComment(jsonbObject)->Comment:
-#     print(type(jsonbObject))
-#     dictionary=json.loads(jsonbObject.decode('utf-8'))
-#     print("_____")
-#     print(dictionary)
-#     comment= Comment(
-#         _id= dictionary["_id"],
-#         userId= dictionary["userId"],
-#         content= dictionary["content"],
-#         itemMusicId=dictionary["itemMusicId"],
-#         likes= dictionary["likes"],
-#         dislikes= dictionary["dislikes"],
-#         createdAt= dictionary["createdAt"],
-#         updatedAt= dictionary["updatedAt"]
-#     )
-#     if "parentId" in dictionary:
-#         comment.parentId=dictionary["parentId"]
-#     return comment
+        return generalRequest(API_URL+"comments/",POST,body=strawberry.asdict(comment))
+    @strawberry.mutation
+    def replyComment(self,parentId:str,reply:ReplyInput) -> Comment:
+        parentComment=generalRequest(API_URL+"comments/{0}".format(parentId),GET)
+        print(parentComment)
+        if(parentComment): #Validate if the comment to be replied actually exists
+            reply.itemMusicId=parentComment["itemMusicId"]
+            reply.parentId=parentId
+            return generalRequest(API_URL+"comments/{0}".format(parentId),POST,body=strawberry.asdict(reply))
+        else:
+            raise Exception("Comment to be replied doesn't exits")
+    @strawberry.mutation
+    def deleteComment(self,id:str) -> DeleteCommentResponse:
+        response=generalRequest(API_URL+"comments/{0}".format(id),DELETE)
+        return response
